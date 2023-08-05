ed28ed01aa1f5b2dda8d21a21a21386096c88b65
Cherry021 <yogacharanreddy@gmail.com>
2023-08-05 12:31:50 +0530

    first commit

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,8 +1,8 @@
 def call(String hubUser, String imageName, String imageTag){
     withCredentials([usernamePassword(credentialsId: 'docker-pass', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "docker login -u '$USER' -p '$PASS'"
+    }
         sh """
             docker image push ${hubUser}/${imageName}:${imageTag} 
         """
-    }
-
 }
